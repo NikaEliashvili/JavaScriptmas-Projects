fa9ccb9afc48a904a783a81afbb9bf1ea8d145ab
NikaEliashvili <nikaeliashvili18@outlook.com>
2023-12-01 15:47:29 +0400

    add Input feature, finished!

--- Day 1 - Countdown to Christmas/index.js
+++ Day 1 - Countdown to Christmas/index.js
@@ -1,30 +1,73 @@
-const countdownDisplay = document.getElementById("countdown-display")
-const countdownHoursDisplay = document.getElementById("countdown-hours-display")
-function renderCountdown(){
+const countdownHeader = document.getElementById("countdown-header");
+const inputHeader = document.getElementById("input-header");
+const inputDate = document.getElementById("input-date");
+const submitBtn = document.getElementById("submit-btn");
+const countdownDisplay = document.getElementById("countdown-display");
+const countdownHoursDisplay = document.getElementById(
+  "countdown-hours-display"
+);
 
-    // Calculate remaining time until Christmas
-    const today = new Date();
-    const christmas = new Date(today.getFullYear(), 11, 25); // Month is 0-based (December is 11)
-    const timeDifference = christmas - today;
+let intervalId;
 
-    // Calculate days, hours, minutes, and seconds
-    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-    
-    countdownDisplay.textContent = days
-    countdownHoursDisplay.textContent = `${hours}:${formatWithLeadingZero(minutes)}:${formatWithLeadingZero(seconds)}`
+function getInputValues() {
+  const userDate = inputDate.value;
+  const defaultChristmasDate = new Date(new Date().getFullYear(), 11, 25);
+
+  // Change Header Dynamically
+  countdownHeader.textContent = inputHeader.value || "Christmas Countdown 🎄";
+
+  // Clear previous interval if any
+  clearInterval(intervalId);
+
+  if (userDate) {
+    // If user provided a date, use it
+    renderCountdown(new Date(userDate));
+  } else {
+    // If no date provided, default to Christmas
+    renderCountdown(defaultChristmasDate);
+  }
+
+  // Set interval to update the countdown every second
+  intervalId = setInterval(function () {
+    if (userDate) {
+      renderCountdown(new Date(userDate));
+    } else {
+      renderCountdown(defaultChristmasDate);
+    }
+  }, 1000);
+}
+
+function renderCountdown(tillSomeDay) {
+  const today = new Date();
+
+  const timeDifference = tillSomeDay - today;
+
+  if (timeDifference < 0) {
+    // Handle case where selected date is in the past
+    countdownDisplay.textContent = 0;
+    countdownHoursDisplay.textContent = "00:00:00";
+    clearInterval(intervalId);
+    return;
+  }
+
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  countdownDisplay.textContent = `${days}`;
+  countdownHoursDisplay.textContent = `${formatWithLeadingZero(
+    hours
+  )}:${formatWithLeadingZero(minutes)}:${formatWithLeadingZero(seconds)}`;
 }
 
 function formatWithLeadingZero(number) {
-    return number < 10 ? `0${number}` : number;
+  return number < 10 ? `0${number}` : number;
 }
 
+submitBtn.addEventListener("click", getInputValues);
 
- 
-setInterval(renderCountdown, 1000)
-
-// Stretch goals:
-// - Display hours, minutes, seconds.
-// - Add a countdown for another festival, your birthday, or Christmas 2022.
+// Initialize with default Christmas countdown
+getInputValues();
